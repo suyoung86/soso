77f3fa24b51bcbc7abcd2959ce1a0630acf05931
Suyoung Ahn <suyoung_an@animal9.net>
2025-04-16 13:10:42 +0900

    test1

---
+++ api/index.ts
@@ -0,0 +1,31 @@
+var express = require("express");
+var app = express();
+
+app.get("/weather", function (req: any, res: any) {
+  const { serviceKey, numOfRows, pageNo, base_date, base_time, nx, ny } =
+    req.query;
+
+  var api_url =
+    "http://apis.data.go.kr/1360000/VilageFcstInfoService_2.0/getUltraSrtNcst?";
+  var request = require("request");
+  var options = {
+    url: api_url,
+    qs: { serviceKey, numOfRows, pageNo, base_date, base_time, nx, ny },
+  };
+
+  request.get(options, function (error: any, response: any, body: any) {
+    if (!error && response.statusCode == 200) {
+      res.writeHead(200, { "Content-Type": "application/xml;charset=utf-8" });
+      res.end(body);
+    } else {
+      res.status(response.statusCode).end();
+      console.log("error = " + response.statusCode);
+    }
+  });
+});
+
+app.listen(3000, function () {
+  console.log(
+    "http://127.0.0.1:3000/weather?serviceKey=내 인증키&numOfRows=10&pageNo=1&base_date=20241028&base_time=0600&nx=61&ny=125 app listening on port 3000!"
+  );
+});
