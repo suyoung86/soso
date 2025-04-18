e0a20b6afbcfe9b713e66baedac43974fdb10676
Suyoung Ahn <suyoung_an@animal9.net>
2025-04-19 07:48:47 +0900

    tes333

---
+++ api/routes/storeZoneOne.js
@@ -0,0 +1,28 @@
+import { Router, Request, Response } from "express";
+import axios from "axios";
+
+const router = Router();
+
+router.get("/soso", async (req: Request, res: Response) => {
+  const { key, servicekey, type } = req.query;
+
+  if (!key || !servicekey || !type) {
+    return res.status(400).json({ message: "Missing required query params" });
+  }
+
+  const apiUrl = "http://apis.data.go.kr/B553077/api/open/sdsc2/storeZoneOne";
+
+  try {
+    const response = await axios.get(apiUrl, {
+      params: { key, servicekey, type },
+      responseType: "text"
+    });
+    res.set("Content-Type", "application/xml;charset=utf-8");
+    res.send(response.data);
+  } catch (error: any) {
+    console.error("API 호출 오류:", error?.response?.status || error.message);
+    res.status(error?.response?.status || 500).send("소상공인 API 호출 오류");
+  }
+});
+
+export default router;
