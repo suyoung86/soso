50b9683ad953a70a2f695d1ea2138bf67a390c8e
Suyoung Ahn <suyoung_an@animal9.net>
2025-04-18 16:50:47 +0900

    test44

--- api/index.ts
+++ api/index.ts
@@ -6,19 +6,13 @@ const app = express();
 app.use(cors()); // CORS 에러 방지용 (프론트와 연동 시 필요)
 
 app.get("/soso/api", async (req: Request, res: Response) => {
-  const { Key, serviceKey, type } =
-    req.query;
+  const { key, serviceKey, type } = req.query;
 
-  if (
-    !key 
-    !serviceKey 
-    !type
-  ) {
+  if (!key || !serviceKey || !type) {
     return res.status(400).json({ message: "Missing required query params" });
   }
 
-  const api_url =
-    "http://apis.data.go.kr/B553077/api/open/sdsc2/storeZoneOne";
+  const api_url = "http://apis.data.go.kr/B553077/api/open/sdsc2/storeZoneOne";
 
   try {
     const response = await axios.get(api_url, {
@@ -34,13 +28,14 @@ app.get("/soso/api", async (req: Request, res: Response) => {
     res.send(response.data);
   } catch (error: any) {
     console.error("API 호출 오류:", error?.response?.status || error.message);
-    res.status(error?.response?.status || 500).send("소상공인 API 호출 오류");
+    res
+      .status(error?.response?.status || 500)
+      .send("소상공인 API 호출 오류");
   }
 });
 
 app.listen(3000, () => {
   console.log(
-    "http://apis.data.go.kr/B553077/api/open/sdsc2/storeZoneOne/soso?key=9174&erviceKey=zWJZJMGi7kBIlToG%2FGVb4RpeALKnhLOKl6B0XRnr%2Bs2w2WtifQ1c8ktURng7cT9gtnNh%2FjkBOrSD0rBqE2mPlA%3D%3D
-&type=xml"
+    "서버 실행 중: http://localhost:3000/soso/api?key=9174&serviceKey=zWJZJMGi7kBIlToG%2FGVb4RpeALKnhLOKl6B0XRnr%2Bs2w2WtifQ1c8ktURng7cT9gtnNh%2FjkBOrSD0rBqE2mPlA%3D%3D&type=xml"
   );
 });
