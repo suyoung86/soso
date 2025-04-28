5c1ed75ec2ea39110bb0917e5d116c92480e3e2f
Suyoung Ahn <suyoung_an@animal9.net>
2025-04-28 11:37:53 +0900

    test123

--- api/services/storeZoneOne.ts
+++ api/services/storeZoneOne.ts
@@ -6,7 +6,11 @@ export const fetchstoreZoneOne = async (query: any) => {
 
   const url = "http://apis.data.go.kr/B553077/api/open/sdsc2/storeZoneOne";
   const response = await axios.get(url, {
-    params: query,
+    params: {
+      // 만약 외부 API가 'ServiceKey'라는 이름을 요구하면 키 이름 변경
+      ServiceKey: query.servicekey,
+      ...query,  // 나머지 파라미터 그대로 전달
+    },
     responseType: "text",
   });
 
