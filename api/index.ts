f11e55834838d01ccf67aa41f65fe17d9c4cc64e
Suyoung Ahn <suyoung_an@animal9.net>
2025-04-28 15:27:54 +0900

    test123

--- api/index.ts
+++ api/index.ts
@@ -5,7 +5,7 @@ dotenv.config();
 
 import express from "express";
 import cors from "cors";
-import sosoRouter from "../routes/sosoRouter"; // ✅ 상대경로 수정
+import sosoRouter from "./routes/sosoRouter"; // ✅ 상대경로 수정
 
 const app = express();
 
