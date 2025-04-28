df4fe122ba44864a80958182618ffcc5e5bb3489
Suyoung Ahn <suyoung_an@animal9.net>
2025-04-28 13:08:14 +0900

    test123

--- api/utils/validateParams.ts
+++ api/utils/validateParams.ts
@@ -2,7 +2,7 @@ export const validateParams = (query: any, required: string[]) => {
   const missing = required.filter((p) => !query[p]);
   if (missing.length > 0) {
     throw {
-      message: `Missing required query params: ${missing.join(", ")}`,
+      message: `Missing required query params 왜왜왜왜오왜왜: ${missing.join(", ")}`,
       status: 400
     };
   }
